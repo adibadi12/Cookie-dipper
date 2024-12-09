17141c309317dc0eba38dde0681f35a18c9b7e1a
adibadi12 <adrian.mathiassen@gmail.com>
2024-12-09 10:37:55 +0100

    added more js and styling

--- script.js
+++ script.js
@@ -4,5 +4,26 @@ const milk = document.getElementById("milk");
 const cookie = document.getElementById("wholeCookie");
  
 cookie.addEventListener("dragstart", (event)=>{
-    event.dataTransfer.setData("text/plain")
-})
+    event.dataTransfer.setData('text/plain', event.target.src);
+    // Lagre bidle kilde i dataTransfer :)
+    event.dataTransfer.effectAllowed = 'copy'; 
+});
+
+const dropZone = addEventListener('dragover', (event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+
+
+});
+
+dropZone.addEventListener('drop', (event) => {
+    event.preventDefault();
+    const cookieSrc = event.dataTransfer.getData('text/plain');
+    const newCookie = document.createElement('img');
+    newCookie.src = cookieSrc;
+    newCookie.alt = 'Dropped Illustration';
+    newCookie.width = 200; // setter størelse
+    dropZone.appendChild(newCookie);
+});
+
+
